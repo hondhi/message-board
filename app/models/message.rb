d45c163b457f2812a524e078146cf95ee369abe1
hondhi <oomasa_danho279@icloud.com>
2016-11-16 11:02:01 +0000

    complete the homework of homessage-board

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,8 @@ class Message < ActiveRecord::Base
   validates :name , length:{maximum:20} , presence: true
   #内容は必要入力かつ2文字以上30文字以内
   validates :body , length:{minimum:2,maximum:30} , presence: true
+  #年齢は0歳以上
+  validates :age, numericality: {
+            only_integer: true, greater_than_or_equal_to: 0, less_than: 100
+          }
 end
